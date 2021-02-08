862968501475d7b0623e03f217a8ff647d770ffe
Rajesh Goyal <zcabrgo@ucl.ac.uk>
2021-02-08 23:40:47 +0000

    searches for skin cancer now, and also alive/dead patients

--- query.py
+++ query.py
@@ -52,7 +52,7 @@ CASE_FIELDS = [
     'demographic.race',
     
     'slide_ids',
-    
+
     'annotations.annotation_id',
     'annotations.category',
     'annotations.classification',
@@ -92,22 +92,72 @@ FILES_ENDPOINT = ENDPOINT + 'files'
 CASE_FILTERS = {
     'op': 'and',
     'content': [
-        { 
+        {
             'op': 'in',
             'content':{
                 'field': 'primary_site',
-                'value': ['Kidney']
+                'value': ['Skin']
             }
         },
-
-        { 
+        {
+            'op': '!=',
+            'content': {
+                'field': 'demographic.vital_status',
+                'value': ['Not Reported']
+            }
+        },
+        {
             'op': 'not',
             'content':{
                 'field': 'slide_ids'
             }
-        },        
+        },
     ]
 }
+
+# NEW_FILTERS = {
+#     "op": "and",
+#       "content": [
+#         {
+#           "op": "in",
+#           "content": {
+#             "field": "cases.primary_site",
+#             "value": [
+#               "skin"
+#             ]
+#           }
+#         },
+#         {
+#           "op": "in",
+#           "content": {
+#             "field": "occurrence.case.case_id",
+#             "value": [
+#               "e5bc45ce-8a14-40b5-b9b5-ce45609fef3a",
+#               "49f961af-1126-431d-93cd-18941c1738f3",
+#               "9817ec15-605a-40db-b848-2199e5ccbb7b",
+#               "beab616a-256e-48f3-a458-028897a6138c",
+#               "af915978-c5e1-4dd4-ab0e-d2be5ac0ae4f",
+#               "449c9d70-d8aa-41d4-beeb-0bbfc73a23d8",
+#               "44b5453a-3009-43b9-bc34-71a11a6d5e63",
+#               "3429967f-4f77-4894-bf27-c4a22698ca92",
+#               "ef608754-3f87-458e-9bcf-4434c54c8c9e",
+#               "83d05b9a-f409-4169-bef9-e772d2cfbfaf",
+#               "ceb45393-54ee-4801-8c6f-c0aa66e37e60",
+#               "590b5e18-d837-4c0e-becf-80520db57c0f",
+#               "4a4f5ee9-b588-4357-b058-effbfb53e2c3",
+#               "dde0b2be-ea43-4de7-8feb-ccdc073c6978",
+#               "46801be1-f035-421f-aae4-81baf3bb8d40",
+#               "d781260c-f969-456e-bf16-044dc5b181a4",
+#               "b8a1732f-c1cb-4a02-af4f-61b63d3d52df",
+#               "1af5d168-a3ff-4648-b195-ede2e7a5ca26",
+#               "0a4b780e-8143-4118-ad98-fd2a2a6678c3",
+#               "d6283ab0-9019-4ad2-93aa-2e6b39cd9641"
+#             ]
+#           }
+#         }
+#       ],
+#     }
+
 
 
 def process_args():
@@ -116,7 +166,7 @@ def process_args():
     ap.add_argument('-O', '--cases_out', help='file to save case query results to', default=None)
     
     ap.add_argument('-N', '--num_cases', help='maximum number of cases to query for', default=50)
-    ap.add_argument('-p', '--primary', help='primary site of the case', default='Kidney')
+    ap.add_argument('-p', '--primary', help='primary site of the case', default='Skin')
     
     ap.add_argument('-x', '--no_files', help='exit without querying slide files', action='store_true')
     ap.add_argument('-n', '--num_slides', help='maximum number of slides to query for', default=None)
@@ -218,7 +268,3 @@ if __name__ == '__main__':
     
     with open(args.slides_out, 'w') as f:
         print(json.dumps(cases, indent=2), file=f)
-    
-    
-    
-    

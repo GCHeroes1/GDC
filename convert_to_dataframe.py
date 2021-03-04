09dfd4ef21d6adab733ec382e95534c6b499cf4c
Nicolas Ford <univnf@gmail.com>
2021-03-04 22:09:14 +0100

    improvements

--- convert_to_dataframe.py
+++ convert_to_dataframe.py
@@ -6,6 +6,9 @@ json_data_path: str = "./finalDataSet.json"
 with open(json_data_path, 'r') as f:
     json_data = json.load(f)
 
+with open("newData.json", 'r') as f:
+    slide_data = json.load(f)
+
 def clean_data(data: dict):
     hits = data["data"]["hits"]
     result = list()
@@ -22,6 +25,24 @@ def clean_data(data: dict):
         result.append(hit)
     return result
 
+
+def clean_data_slides(data: dict):
+    hits = data["data"]
+    result = list()
+    for hit in hits:
+        # make data more readable
+        hit.update(hit["slides"])
+        # remove unnecessary data
+        hit.pop("slides")
+        print(hit["SampleCode"])
+        if hit["SampleCode"] == "01":
+            hit["sample_type"] = "Primary Solid Tumor"
+        elif hit["SampleCode"] == "06":
+            hit["sample_type"] = "Metastatic"
+        print(hit)
+        result.append(hit)
+    return result
+
 def to_dataframe(data: dict):
     dataframe = pd.DataFrame(data)
     return dataframe
@@ -31,3 +52,7 @@ cleaned_data = clean_data(json_data)
 dataframe: pd.DataFrame = to_dataframe(cleaned_data)
 print(dataframe)
 dataframe.to_csv(path_or_buf="readable_data.csv")
+
+slide_cleaned_data = clean_data_slides(slide_data)
+slide_dataframe = to_dataframe(slide_cleaned_data)
+slide_dataframe.to_csv("slide_data.csv")

8f5f910b1ea1fa1ab0e3ee8d609f2ef96f70c9e8
Nicolas Ford <univnf@gmail.com>
2021-03-05 01:22:25 +0100

    updated

--- convert_to_dataframe.py
+++ convert_to_dataframe.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import json
 
-json_data_path: str = "./finalDataSet.json"
+json_data_path: str = "./reconstructedData.json"
 
 with open(json_data_path, 'r') as f:
     json_data = json.load(f)
@@ -9,50 +9,63 @@ with open(json_data_path, 'r') as f:
 with open("newData.json", 'r') as f:
     slide_data = json.load(f)
 
-def clean_data(data: dict):
-    hits = data["data"]["hits"]
+def clean_slide_data(data: dict):
+    hits = data["data"]
     result = list()
     for hit in hits:
-        # make data more readable
-        hit.update(hit["demographic"])
-        hit["number_of_slides"] = len(hit["slides"])
-        # hit["diagnoses"] = [entry["primary_diagnosis"] for entry in hit["diagnoses"]]
+        hit.update()
 
-        # remove unnecessary data
-        hit.pop("slide_ids")
-        # hit.pop("slides")
-        hit.pop("demographic")
-        result.append(hit)
+        # add useful field for when reading data visually
+        sample_code = hit["slides"]["SampleCode"]
+        if sample_code == "01":
+            hit["slides"]["sample_type"] = "Primary Solid Tumor"
+        elif sample_code == "06":
+            hit["slides"]["sample_type"] = "Metastatic"
+        elif sample_code == "07":
+            hit["slides"]["sample_type"] = "Additional Metastatic"
+        elif sample_code == "11":
+            hit["slides"]["sample_type"] = "Solid Tissue Normal"
+
+        result.append(hit["slides"])
     return result
 
-
-def clean_data_slides(data: dict):
+# works on reconstructedData.json
+def clean_data(data: dict):
     hits = data["data"]
     result = list()
     for hit in hits:
         # make data more readable
-        hit.update(hit["slides"])
+        # hit.update(hit["slides"])
+
+        # # add useful field for when reading data visually
+        # sample_code = hit["slides"]["SampleCode"]
+        # if sample_code == "01":
+        #     hit["sample_type"] = "Primary Solid Tumor"
+        # elif sample_code == "06":
+        #     hit["sample_type"] = "Metastatic"
+
         # remove unnecessary data
         hit.pop("slides")
-        print(hit["SampleCode"])
-        if hit["SampleCode"] == "01":
-            hit["sample_type"] = "Primary Solid Tumor"
-        elif hit["SampleCode"] == "06":
-            hit["sample_type"] = "Metastatic"
-        print(hit)
+
+        # print(hit)
         result.append(hit)
     return result
+
 
 def to_dataframe(data: dict):
     dataframe = pd.DataFrame(data)
     return dataframe
 
 
+# reconstructed Data dataframe
 cleaned_data = clean_data(json_data)
 dataframe: pd.DataFrame = to_dataframe(cleaned_data)
 print(dataframe)
+print("saving main dataframe to csv")
 dataframe.to_csv(path_or_buf="readable_data.csv")
 
-slide_cleaned_data = clean_data_slides(slide_data)
+# slides dataframe
+slide_cleaned_data = clean_slide_data(slide_data)
 slide_dataframe = to_dataframe(slide_cleaned_data)
+print("saving slide dataframe to csv")
 slide_dataframe.to_csv("slide_data.csv")

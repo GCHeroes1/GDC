22b1efdd05c6e3ed9edf3e88ef2d7c7fb33be429
Nicolas Ford <univnf@gmail.com>
2021-03-05 01:57:37 +0100

    fixed bugs

--- convert_to_dataframe.py
+++ convert_to_dataframe.py
@@ -1,22 +1,23 @@
 import pandas as pd
 import json
+import copy
 
 json_data_path: str = "./reconstructedData.json"
 
 with open(json_data_path, 'r') as f:
     json_data = json.load(f)
 
-with open("test3.json", 'r') as f:
-    slide_data = json.load(f)
+# with open("test3.json", 'r') as f:
+#     slide_data = json.load(f)
 
 def clean_slide_data(data: dict):
-    hits = data["data"]
+    hits = copy.deepcopy(data["data"])
     result = list()
     for hit in hits:
-        hit.update()
-
         # add useful field for when reading data visually
-        sample_code = hit["slides"]["SampleCode"]
+        sample_code = hit["slides"]["SampleCode"] if "slides" in hit and "SampleCode" in hit["slides"] else None
+        if sample_code == None:
+            hit["slides"]["sample_type"] = "No sample_type"
         if sample_code == "01":
             hit["slides"]["sample_type"] = "Primary Solid Tumor"
         elif sample_code == "06":
@@ -29,9 +30,10 @@ def clean_slide_data(data: dict):
         result.append(hit["slides"])
     return result
 
+
 # works on reconstructedData.json
 def clean_data(data: dict):
-    hits = data["data"]
+    hits = copy.deepcopy(data["data"])
     result = list()
     for hit in hits:
         # make data more readable
@@ -65,7 +67,7 @@ print("saving main dataframe to csv")
 dataframe.to_csv(path_or_buf="readable_data.csv")
 
 # slides dataframe
-slide_cleaned_data = clean_slide_data(slide_data)
+slide_cleaned_data = clean_slide_data(json_data)
 slide_dataframe = to_dataframe(slide_cleaned_data)
 print("saving slide dataframe to csv")
 slide_dataframe.to_csv("slide_data.csv")

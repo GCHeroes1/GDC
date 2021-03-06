f5d2135946deb921f2b3d4df61e8717ee2b320d0
Rajesh Goyal <zcabrgo@ucl.ac.uk>
2021-03-06 12:43:35 +0000

    download works locally for me but its slow

--- download_slides.py
+++ download_slides.py
@@ -2,6 +2,37 @@ import requests
 import json
 import sys
 from pathlib import Path
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+import os
+import shutil
+# import rclone
+import subprocess
+from tqdm import tqdm
+
+clientID = "1006106238032-pkeodfsvnfl5ed84bcdkh2uv5f1f75e9.apps.googleusercontent.com"
+clientSecret = "Yg9aCQnU-MtqK1lVtlitgiOl"
+gauth = GoogleAuth()
+drive = GoogleDrive(gauth)
+
+gfile = drive.CreateFile({'parents': [{'id': '1_ft4g8_ehX_XUn5T-vySvguKssWEOztP'}]})
+
+# cfg_path = "rclone.conf"
+# with open(cfg_path) as f:
+#    cfg = f.read()
+# print(cfg)
+# result = rclone.with_config(cfg).listremotes()
+# print(result)
+# result.listremotes()
+# print(result.get('out'))
+# result.run_cmd("rclone config")
+
+# cfg = """[local]
+# type = local
+# nounc = true"""
+# result = rclone.with_config(cfg).listremotes()
+
+# print(result.get('code'))
 
 path_to_slides_data = "reconstructedData.json"
 endpoint = "https://api.gdc.cancer.gov/data/"
@@ -11,48 +42,59 @@ with open(path_to_slides_data, 'r') as f:
 
 def download_slide(hit_id, slide_id, file_id):
     output_path = f"slides/{hit_id}/{file_id}/"
-    Path(output_path).mkdir(parents=True, exist_ok=True)    # create folders if they don't exist
+    Path(output_path).mkdir(parents=True, exist_ok=True)  # create folders if they don't exist
 
     r = requests.get(endpoint + file_id, stream=True)
     total_length = r.headers.get('content-length')
 
     with open(output_path + file_id + ".svs", "wb") as f:
-        # download progress bar
-        if total_length is None: # no content length header
+        if total_length is None:  # no content length header
             f.write(r.content)
         else:
-            dl = 0
-            total_length = int(total_length)
-            for data in r.iter_content(chunk_size=4096):
-                dl += len(data)
-                f.write(data)
-                done = int(50 * dl / total_length)
-                sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50-done)) )
-                sys.stdout.flush()
-            print()
-
+            chunk_size = 1024 * 1024
+            for content_chunk in tqdm(r.iter_content(chunk_size=chunk_size), total=int(total_length)//chunk_size):
+                f.write(content_chunk)
 
 # print(slides_data)
 
 patients = slides_data["data"]  # all slides
-
+x = 0
 for patient in patients:
     patientID = patient["patient_id"]
     data = patient["slides"]
+    case_id = data["case_id"]
+    file_id = data["file_id"]
     if "slide_id" not in data:
         print("slide ID for " + patientID + " is missing")
-        sys.exit()
+        continue
     if "case_id" not in data:
         print("case ID for " + patientID + " is missing")
-        sys.exit()
+        continue
     if "file_id" not in data:
         print("file ID for " + patientID + " is missing")
-        sys.exit()
-    print(f"Getting Slides for case id: " + data["case_id"])
-    print(f"    downloading slide with id " + data["slide_id"] + " and file id " + data["file_id"])
-    download_slide(data["case_id"], data["slide_id"], data["file_id"])
-    sys.exit()
+        continue
+    print(f"Getting Slides for case id: " + case_id)
+    print(f"    downloading slide with id " + data["slide_id"] + " and file id " + file_id)
+    download_slide(case_id, data["slide_id"], file_id)
 
+    filepath = "slides/" + case_id + "/" + file_id + "/"
+    filename = filepath + file_id + ".svs"
+    rclone_path = "/home/rajesh/Downloads/rclone-v1.54.0-linux-amd64/rclone"
+    data_path = "/home/rajesh/Documents/Year_3/COMP0031/gdc/" + filename
+    print(data_path)
 
+    subprocess.run([rclone_path, "copy", data_path, "GCDData:/Data", "-Pv", "--max-size", "1M", "--transfers=16"])
 
+    # gfile.SetContentFile(filename)
+    # gfile.Upload()
+    # print(filename)
+
+    if os.path.exists(filename):
+        # print("yep it exists")
+        shutil.rmtree("slides")
+    else:
+        print("file dont exist m8")
+    # sys.exit()
+    x+=1
+    print("i have done this for the " + str(x) + " time")
 
